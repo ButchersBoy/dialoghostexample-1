21984e1dbb7a432afc26f8bfe4ab11ae8e561eee
Thiago Carvalho <thiago.carvalho@escriba.com.br>
2017-02-13 09:25:10 -0200

    Initial commit

---
+++ DialogHostTest/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using MaterialDesignThemes.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DialogHostTest
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ViewMessage view = new ViewMessage();
+
+            var result = await DialogHost.Show(view);
+        }
+
+        private void Window_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.F5)
+            {
+                this.Button_Click(sender, new RoutedEventArgs());
+            }
+        }
+    }
+}
